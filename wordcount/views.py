540885ecf5a828d2ec2ba03f0c028e8377770e80
wjh <wjh.qq.com>
2019-06-17 16:03:38 +0800

    词频统计

--- wordcount/views.py
+++ wordcount/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'home.html')
+
+def count(request):
+    text=request.GET['text']
+    print(text)
+    result={}
+    for i in text:
+        if i not in result:
+            result[i]=1
+        else:
+            result[i]+=1
+    print(result)
+
+    return render(request,'count.html',{'count_result':result.items()})
